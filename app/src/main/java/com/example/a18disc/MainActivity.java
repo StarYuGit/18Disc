0da2a42d9e88e11f0c25f904411eca80f7230036
staryu <japenjustnew@gmail.com>
2020-05-25 16:28:08 +0800

    Version 1

---
+++ app/src/main/java/com/example/a18disc/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.a18disc;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
